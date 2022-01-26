06a7b9076f59efb7727c19c995fa97299ee90178
Gaetan Jonathan <gaetan.s118@gmail.com>
2022-01-26 09:49:29 +0300

    Refont Structure

--- src/controllers/core.py
+++ src/controllers/core.py
@@ -1,9 +1,16 @@
+from flask import Config
 import ampalibe
+
 
 '''
     Principal fonction, où les messages réçu sur
     la page facebook rentrent.
+
+    @param user_id: identifiant facebook de l'exp.
+    @param cmd: message de la personne.
+    @param extends: contiennent la liste des autres
+        données envoyé par facevook (heure d'envoi, ...)
 '''
 @ampalibe.main
-def default(user_id, cmd, **extends):
+def default(user_id, message, **extends):
     pass

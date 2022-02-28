12d985a57c1aa918d086bda27f28db7e04bc492a
Gaetan Jonathan <gaetan.s118@gmail.com>
2022-02-28 16:33:25 +0300

    [FIX] Ampalibe source not defined

--- bin/ampalibe.bat
+++ bin/ampalibe.bat
@@ -2,16 +2,16 @@
 
 IF /I "%1" == "create" (
     md %2
-    python -c "import ampalibe;print(ampalibe.source.env_cmd)" > %2\.env.bat
-    python -c "import ampalibe;print(ampalibe.source.conf)" > %2\conf.py
-    python -c "import ampalibe;print(ampalibe.source.core)" > %2\core.py
+    python -c "import ampalibe.source;print(ampalibe.source.env_cmd)" > %2\.env.bat
+    python -c "import ampalibe.source;print(ampalibe.source.conf)" > %2\conf.py
+    python -c "import ampalibe.source;print(ampalibe.source.core)" > %2\core.py
     md %2\assets\public
     md %2\assets\private
 )
 IF /I "%1" == "init" (
-    python -c "import ampalibe;print(ampalibe.source.env_cmd)" > .env.bat
-    python -c "import ampalibe;print(ampalibe.source.conf)" > conf.py
-    python -c "import ampalibe;print(ampalibe.source.core)" > core.py
+    python -c "import ampalibe.source;print(ampalibe.source.env_cmd)" > .env.bat
+    python -c "import ampalibe.source;print(ampalibe.source.conf)" > conf.py
+    python -c "import ampalibe.source;print(ampalibe.source.core)" > core.py
     md assets\public
     md assets\private
 )

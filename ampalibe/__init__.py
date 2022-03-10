20bbb57feddf2773a31ea1ba77e55799d2bc0f50
Gaetan Jonathan <gaetan.s118@gmail.com>
2022-03-10 15:22:53 +0300

    [FIX] Payload not defined

--- ampalibe/__init__.py
+++ ampalibe/__init__.py
@@ -1,7 +1,7 @@
 from .requete import Model
 from .messenger import Messenger
-from .utils import action, command
 from .core import webserver, Extra as init
+from .utils import action, command, Payload
 
 __version__ = '0.1.0-dev'
 __author__ = 'iTeam-$'

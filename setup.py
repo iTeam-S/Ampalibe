328364bd440201389ac793e99abb90ac6edacaa0
Gaetan Jonathan <gaetan.s118@gmail.com>
2022-04-08 15:19:21 +0300

    [IMP] Add python 3.6 support

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setuptools.setup(
     packages=['ampalibe'],  # List of all modules to be installed
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
@@ -21,7 +22,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],                      # Information to filter the project on PyPi website
-    python_requires='>=3.7',
+    python_requires='>=3.6',
     py_modules=["ampalibe"],                      # Name of the python package
     install_requires=[
         "fastapi", "uvicorn", "mysql-connector",

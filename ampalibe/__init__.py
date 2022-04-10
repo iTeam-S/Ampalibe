121742dde3fb996cdb5b3b7f94417b361e9fff29
Gaetan Jonathan <gaetan.s118@gmail.com>
2022-04-10 07:31:13 +0300

    [REL] 1.0.4

--- ampalibe/__init__.py
+++ ampalibe/__init__.py
@@ -3,5 +3,5 @@ from .messenger import Messenger
 from .core import webserver, Extra as init
 from .utils import action, command, download_file, Payload
 
-__version__ = '1.0.4.a'
+__version__ = '1.0.4'
 __author__ = 'iTeam-$'

e58c39ea16be319858f17652cb4a3d339fd3c99e
Gaetan Jonathan <gaetan.s118@gmail.com>
2022-04-13 10:14:37 +0300

    Improve Script
    - Linux: Add env help in usage
    
    - remove unecessary .env file in pattern

--- bin/ampalibe.bat
+++ bin/ampalibe.bat
@@ -24,7 +24,7 @@ IF /I "%1" == "init" (
 IF /I "%1" == "run" (
     call .env.bat
     IF /I "%2" == "--dev" (
-        watchmedo auto-restart --patterns="*.py;.env;.env.bat" --recursive -- python -c "import core;core.ampalibe.init.run(core.Configuration())"
+        watchmedo auto-restart --patterns="*.py;.env.bat" --recursive -- python -c "import core;core.ampalibe.init.run(core.Configuration())"
         exit
     )
     python -c "import core;core.ampalibe.init.run(core.Configuration())"

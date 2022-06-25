77aa55190afbe26c9831d64638f1c86bb50f5c44
Gaetan Jonathan <gaetan.s118@gmail.com>
2022-06-25 05:17:36 +0300

    [ADD] Simulate a message
    You can now simulate a message by user.

--- tests/test.py
+++ tests/test.py
@@ -1,35 +1,3 @@
-import requests
-
-def simulate(mes, user):
-    '''
-        Simulaion d'envoie des données
-    '''
-    data_json = {
-        "object": "page",
-        "entry": [
-            {
-                "messaging": [
-                    {
-                        "message":
-                            {
-                                "text": mes,
-                            },
-                        "sender":
-                            {
-                                "id": user
-                            }
-                    }
-                ]
-            }
-        ]
-    }
-    header = {'content-type': 'application/json; charset=utf-8'}
-    return requests.post(
-        'http://127.0.0.1:4555',
-        json=data_json,
-        headers=header,
-        params={"TEST": '1'}
-    )
-
-
-print(simulate("/traduire", "4475829389183936").text)
+'''
+Comming Soon
+'''

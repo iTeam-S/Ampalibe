584fc5c1646b6e223473bba2befeb49357e46968
Gaetan Jonathan <gaetan.s118@gmail.com>
2022-08-07 20:23:11 +0300

    view log

--- automate/deploy.sh
+++ automate/deploy.sh
@@ -4,6 +4,8 @@
 docker build . -t ampalibe
 
 # run a container
-docker run -d -v "${PWD}/example/default:/usr/src/app" -p 4555:4555 ampalibe
+docker run -d -v "${PWD}/example/default:/usr/src/app" -p 4555:4555 --name amp ampalibe
 
+sleep 2 
 
+docker logs amp

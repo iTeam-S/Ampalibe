97c187bd03c509b448fb19f4fdedc62495a5a9c3
Gaetan Jonathan <gaetan.s118@gmail.com>
2022-08-07 20:36:52 +0300

    [flake8] error

--- automate/deploy.sh
+++ automate/deploy.sh
@@ -6,6 +6,5 @@ docker build . -t ampalibe
 # run a container
 docker run -d -v "${PWD}/example/default:/usr/src/app" -p 4555:4555 --name amp ampalibe
 
-sleep 2 
-
-docker logs amp
+# Attente de stabilité du serveur
+sleep 2

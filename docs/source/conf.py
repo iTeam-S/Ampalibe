c5c78222a27fd5ffeb3751b1bdc427cd65ddab9a
Gaetan Jonathan <gaetan.s118@gmail.com>
2022-08-08 15:06:21 +0300

    last fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,7 +2,7 @@
 import os
 import sys
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(".."))))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath("."))))
 
 
 current_path = os.path.dirname(os.path.abspath(__file__))

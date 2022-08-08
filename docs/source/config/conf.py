e81988329ae2f8ebe2baa3640c352969bba69088
Gaetan Jonathan <gaetan.s118@gmail.com>
2022-08-08 14:43:07 +0300

    hot debug

--- docs/source/config/conf.py
+++ docs/source/config/conf.py
@@ -3,6 +3,8 @@ import os
 import sys
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(".."))))
+print(sys.path)
+sys.path.remove(os.path.dirname(os.path.abspath(__file__)))
 import ampalibe
 
 # -- Project information

f0d16064b0c9743f5616c3f2ae7d0bbafb609e1e
Gaetan Jonathan <gaetan.s118@gmail.com>
2022-09-01 20:54:44 +0300

    [IMP] Improved directory name

--- tests/deploy.sh
+++ tests/deploy.sh
@@ -4,7 +4,7 @@
 docker build . -t ampalibe
 
 # run a container
-docker run -d -v "${PWD}/automate/app:/usr/src/app" -p 4555:4555 --name amp ampalibe
+docker run -d -v "${PWD}/tests/app:/usr/src/app" -p 4555:4555 --name amp ampalibe
 
 # Attente de stabilité du serveur
 sleep 2

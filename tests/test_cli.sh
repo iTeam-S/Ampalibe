14e291204cc19975236d0243d7d4879d281b313e
Gaetan Jonathan <gaetan.s118@gmail.com>
2022-09-29 00:17:18 +0300

    [ADD] Unit Test for ampalibe CLI
    - test ampalibe create
    - test ampalibe init
    - test ampalibe lang
    - test ampalibe env
    - test apalibe version

---
+++ tests/test_cli.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+verif_content(){
+    for file in core.py .env conf.py langs.json .gitignore requirements.txt
+    do 
+        test -f $1/$file || { echo "$file not found" ; exit 1; }
+    done
+    test -d $1/assets/public || { echo "assets/public not found" ; exit 1; }
+    test -d $1/assets/private || { echo "assets/private in assets not found" ; exit 1; }
+}
+
+#### TEST AMPALIBE CREATE #######
+ampalibe create test_proj > /dev/null
+verif_content test_proj
+rm -r test_proj
+
+#### TEST AMPALIBE INIT #######
+mkdir test_proj 
+cd test_proj 
+ampalibe init > /dev/null
+verif_content .
+
+rm langs.json .env 
+#### TEST AMPALIBE LANG #####
+ampalibe lang > /dev/null
+test -f langs.json || { echo "ampalibe lang not working" ; exit 1; }
+
+#### TEST AMPALIBE ENV #####
+ampalibe env > /dev/null
+test -f .env || { echo "ampalibe env not working" ; exit 1; }
+
+cd ..
+rm -r test_proj
+
+#### TEST AMPALIBE VERSION
+ampalibe version > /dev/null || { echo "ampalibe version not working" ; exit 1; }

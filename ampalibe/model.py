593072f9a5fb742be47027cbc8bc2ddf746d2859
Gaetan Jonathan <gaetan.s118@gmail.com>
2022-12-05 06:31:48 +0300

    [IMP]] Make Sqlite support type datetime

--- ampalibe/model.py
+++ ampalibe/model.py
@@ -45,7 +45,10 @@ class Model:
         else:
             import sqlite3
 
-            self.db = sqlite3.connect(self.DB_CONF)
+            self.db = sqlite3.connect(
+                self.DB_CONF, 
+                detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES
+            )
 
         self.cursor = self.db.cursor()
 

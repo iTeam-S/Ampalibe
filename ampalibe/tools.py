fb84687cec5505af6a8653ba68c8a39c3821f750
Gaetan Jonathan <gaetan.s118@gmail.com>
2023-01-09 07:57:59 +0300

    separate decorators and tools

--- ampalibe/tools.py
+++ ampalibe/tools.py
@@ -82,66 +82,6 @@ def analyse(data):
     return None, Cmd(""), None
 
 
-def command(*args, **kwargs):
-    """
-    A decorator that registers the function as the route
-        of a processing per command sent.
-    """
-
-    def call_fn(function):
-        funcs["command"][args[0]] = function
-
-    return call_fn
-
-
-def action(*args, **kwargs):
-    """
-    A decorator that registers the function as the route
-        of a defined action handler.
-    """
-
-    def call_fn(function):
-        funcs["action"][args[0]] = function
-
-    return call_fn
-
-
-def event(*args, **kwargs):
-    """
-    A decorator that registers the function as the route
-        of a defined event handler.
-    """
-
-    def call_fn(function):
-        funcs["event"][args[0]] = function
-
-    return call_fn
-
-
-def before_receive(*args, **kwargs):
-    """
-    A decorator that run the function before
-        running apropriate function
-    """
-
-    def call_fn(function):
-        funcs["before"] = function
-
-    return call_fn
-
-
-def after_receive(*args, **kwargs):
-    """
-    A decorator that run the function after
-        running apropriate function
-    """
-
-    def call_fn(function):
-        funcs["after"] = function
-
-    return call_fn
-
-
 def before_run(func, **kwargs):
     res = None
     if funcs["before"] and hasattr(funcs["before"], "__call__"):

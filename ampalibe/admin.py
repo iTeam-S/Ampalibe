c09e26b025ffa6fdbc83e3ebe54405457d7581a4
Gaetan Jonathan <gaetan.s118@gmail.com>
2023-02-22 07:30:04 +0300

    Add log admin by default

--- ampalibe/admin.py
+++ ampalibe/admin.py
@@ -2,12 +2,13 @@ import inspect
 import hashlib
 from .logger import Logger
 from typing import Optional
+from datetime import datetime
 from .model import DataBaseConfig
 from .constant import AMPALIBE_LOGO
 from sqladmin import Admin, ModelView
 from starlette.requests import Request
 from sqladmin.authentication import AuthenticationBackend
-from sqlmodel import create_engine, SQLModel, Field, Session, select
+from sqlmodel import select, create_engine, Field, Session, SQLModel
 
 
 engine = create_engine(
@@ -15,16 +16,23 @@ engine = create_engine(
 )
 
 
-
 class AdminModel(SQLModel, table=True):
     __tablename__ = "admin_user"
     id: Optional[int] = Field(default=None, primary_key=True)
     username: str = Field(max_length=50, unique=True, nullable=False)
     password: str = None
+    created_at: datetime = Field(default=datetime.utcnow(), nullable=False)
+    last_edited: datetime = Field(default_factory=datetime.utcnow, nullable=False)
 
 
 class AdminUser(ModelView, model=AdminModel):
-    column_list = [AdminModel.id, AdminModel.username]
+    column_list = [
+        AdminModel.id,
+        AdminModel.username,
+        AdminModel.password,
+        AdminModel.last_edited,
+        AdminModel.created_at,
+    ]
     icon = "fa-solid fa-user-secret"
     can_export = False
 
@@ -39,12 +47,14 @@ class AdminUser(ModelView, model=AdminModel):
             statement = select(AdminModel).where(AdminModel.id == model.id)
             results = session.exec(statement)
             admin_model = results.one()
-            admin_model.username = data.get('username')
-            admin_model.password = hashlib.sha256(data.get('password').encode()).hexdigest()
+            admin_model.username = data.get("username")
+            admin_model.password = hashlib.sha256(
+                data.get("password").encode()
+            ).hexdigest()
             session.add(admin_model)
             session.commit()
             session.refresh(admin_model)
-    
+
 
 class AdminAuth(AuthenticationBackend):
     async def login(self, request: Request) -> bool:
@@ -52,8 +62,13 @@ class AdminAuth(AuthenticationBackend):
         username, password = form["username"], form["password"]
 
         with Session(engine) as session:
-            statement = select(AdminModel).where(AdminModel.username == username)
-            statement = statement.where(AdminModel.password == hashlib.sha256(password.encode()).hexdigest())
+            statement = (
+                select(AdminModel)
+                .where(AdminModel.username == username)
+                .where(
+                    AdminModel.password == hashlib.sha256(password.encode()).hexdigest()
+                )
+            )
             results = session.exec(statement)
             verif = len(results.all()) > 0
 
@@ -61,38 +76,56 @@ class AdminAuth(AuthenticationBackend):
             request.session.update({"token": "..."})
         return verif
 
-
     async def logout(self, request: Request) -> bool:
         request.session.clear()
         return True
 
     async def authenticate(self, request: Request) -> bool:
         token = request.session.get("token")
-        return True if token else False
+        if not token:
+            with Session(engine) as session:
+                statement = select(AdminModel)
+                results = session.exec(statement)
+                if len(results.all()) == 0:
+                    session.add(
+                        AdminModel(
+                            username="admin",
+                            password=hashlib.sha256(b"ampalibe").hexdigest(),
+                        )
+                    )
+                    session.commit()
+            return False
+        return True
 
 
 def init_admin(app):
     views = get_user_resources()
     SQLModel.metadata.create_all(engine)
-    admin = Admin(app, engine, title='Ampalibe Admin', logo_url=AMPALIBE_LOGO, authentication_backend = AdminAuth(secret_key="..."))
+    admin = Admin(
+        app,
+        engine,
+        title="Ampalibe Admin",
+        logo_url=AMPALIBE_LOGO,
+        authentication_backend=AdminAuth(secret_key="..."),
+    )
     for view in views:
         admin.add_view(view)
-    
-    
+
+
 def get_user_resources():
     allViews = [AdminUser]
     try:
         import resources  # type: ignore
+
         clsmembers = inspect.getmembers(resources, inspect.isclass)
         for name, obj in clsmembers:
-            if name == 'ModelView':
+            if name == "ModelView":
                 continue
             if issubclass(obj, ModelView):
                 allViews.append(obj)
-    
+
     except Exception as err:
         Logger.warning(err)
-        
+
     finally:
         return allViews
-    
